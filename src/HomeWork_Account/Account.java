d327acc33fbfd83adbd1982175afd61e35cbe043
Liliia Baieva <555blv@gmail.com>
2023-03-17 13:25:53 +0100

    add Account

---
+++ src/HomeWork_Account/Account.java
@@ -0,0 +1,72 @@
+package HomeWork_Account;
+
+public class Account {
+
+  private final String name;
+  private final String email;
+  private int reputation;
+
+  public Account(String name, String email) {
+    this.name = name;
+    this.email = email;
+    reputation = 0;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  /**
+   * Увеличивает репутацию.
+   *
+   * @param  diff
+   *         Разница между старым и новым значением, всегда положительная
+   */
+  public void increaseReputation(int diff) {
+    if (diff > 0) {
+      reputation += diff;
+    }
+  }
+
+  /**
+   * Уменьшает репутацию.
+   *
+   * @param  diff
+   *         Разница между старым и новым значением, всегда положительная
+   */
+  public void decreaseReputation(int diff) {
+    if (diff > 0) {
+      reputation -= diff;
+    }
+  }
+
+  public int getReputation() {
+    return reputation;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("name: %s, e-mail: %s, reputation: %s", name, email, reputation);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) { // ссылки this и obj ведут на один и тот же объект
+      return true; // см. правило 1
+    }
+    if (! (obj instanceof Account other)) {
+      return false;
+    }
+
+    return this.name.equalsIgnoreCase(other.name) && this.email.equalsIgnoreCase(other.email);
+  }
+
+  @Override
+  public int hashCode() {
+    return name.hashCode() * email.hashCode();
+  }
+}
